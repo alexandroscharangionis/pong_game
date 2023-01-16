75849b652b311289dfb9f7dfd50593afd59ff1de
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-16 12:28:47 +0200

    initialised both paddles + added key listeners for each paddle + created game loop based on boolean game_is_on variable

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.bgcolor("gold")
+screen.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+screen.listen()
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
