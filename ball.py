49870ff7c1b1ebb1d220669750bab6509190fffc
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-16 12:57:14 +0200

    created dedicated variables for x axis and y axis movement

--- ball.py
+++ ball.py
@@ -7,8 +7,10 @@ class Ball(Turtle):
         self.color("black")
         self.shape("circle")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
