97819412d55ac9ee655fd57603421efb989e730d
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-16 12:27:33 +0200

    refactored class to inherit from Turtle

--- paddle.py
+++ paddle.py
@@ -1,21 +1,18 @@
 from turtle import Turtle
 
 
-class Paddle:
-    def __init__(self):
-        paddle1 = self.create_paddle(350, 0)
-        paddle2 = self.create_paddle(-350, 0)
-
-    def create_paddle(self, x_pos, y_pos):
-        width = 20
-        height = 100
-        paddle = Turtle("square")
-        paddle.resizemode("user")
-        paddle.shapesize(stretch_wid=5, stretch_len=1)
-        paddle.color("black")
-        paddle.speed("fastest")
-        paddle.penup()
-        paddle.goto(x_pos, y_pos)
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
 
     def up(self):
-        pass
+        new_y_pos = self.ycor() + 20
+        self.goto(self.xcor(), new_y_pos)
+
+    def down(self):
+        new_y_pos = self.ycor() - 20
+        self.goto(self.xcor(), new_y_pos)
